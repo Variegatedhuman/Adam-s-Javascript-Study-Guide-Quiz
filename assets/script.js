ec66abb9621fb933f1c55d24d79912204c1628d5
Variegatedhuman <116999437+Variegatedhuman@users.noreply.github.com>
2022-12-18 22:07:22 -0600

    updates to html, css, and js

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,54 @@
 const startButton = document.querySelector("#start-button")
-const buttonOne = document.querySelector("#choice-1")
-const buttonTwo = document.querySelector("#choice-2")
-const buttonThree = document.querySelector("#choice-3")
-const buttonFour = document.querySelector("#choice-4")
-const scoreButton = document.querySelector(".highScoreLeaderboard")
+const leaderBoard = document.querySelector(".highScoreLeaderboard")
 const questions = document.querySelector(".questions")
 const questionCard = document.querySelector(".questionCards")
 const currentTime = document.querySelector("#currentTime")
+const initialButton = document.querySelector("#initialButton")
+const scoreBoard = document.querySelector(".scoreBoard")
+const initials = document.querySelector("#initialText")
 let currentQuestion = 0
 let score = 0
-let time = 5
+let time = 60
 let timer 
+
+alert("Welcome! You'll have 60 seconds to complete this quiz. If you get a question wrong then you'll have 10 seconds deducted. Click start when you are ready to begin.")
+
 
 var question = [
     {
-        questionTitle: "Favorite fruit?",
+        questionTitle: "Some'll win, some will lose Some are born to sing the blues Whoa, the movie never ends It goes on and on and on and on?",
         choices: ["apple", "oranges", "peaches", "grapefruit"],
         answer: "apple"
     },
-    {
-        questionTitle: "Favorite veggie?",
-        choices: ["carrot", "onions", "eggplant", "celery"],
-        answer: "carrot"
-    },
-    {
-        questionTitle: "Favorite rock?",
-        choices: ["granite", "limestone", "tigerseye", "marble"],
-        answer: "granite"
-    },
+    
 ]
+
+
+
+
+
+// function leaders(){
+//     const scoreLeaders = document.querySelector(".scoreLeaders")
+//     scoreLeaders.innerHTML="";
+//         var scoreLeadersEl= document.createElement("li");
+//         scoreLeadersEl.textContent = localStorage.getItem(score)
+//         scoreLeadersEl.textContent = localStorage.getItem(initials)
+//         leaderBoard.appendChild(scoreLeadersEl)
+
+// }
+
+
 
 function endQuiz (){
     questionCard.classList.add("hide");
-    const scoreBoard = document.querySelector(".scoreBoard")
     scoreBoard.classList.remove("hide")
     let scoreEl = document.createElement("h2")
     scoreEl.textContent = "Congrats your score is: " + score
     scoreBoard.appendChild(scoreEl)
     clearInterval(timer)
-
+    // localStorage.setItem(initials);
+    // localStorage.setItem(score)
+    // leaders()
 }
 
 function checkAnswer(){
@@ -94,5 +104,35 @@ function startQuiz() {
     timerCountdown();
 }
 
+function openLeaderBoard(){
+    scoreBoard.classList.add("hide")
+    leaderBoard.classList.remove("hide")
+    
+}
+
+function saveUserScore() {
+    var userScore = {
+      user: initials.value,
+      currentScore: score.value,
+    };
+    localStorage.setItem("userScore", JSON.stringify(userScore));
+  }
+  
+  function renderLastScore() {
+    var lastScore = JSON.parse(localStorage.getItem("userScore"));
+    if (lastScore !== null) {
+    document.getElementsByClassName("user-Score").innerHTML = lastScore.user;
+    document.getElementsByClassName("user-Score").innerHTML = lastScore.currentScore;
+    } else {
+      return;
+    }
+  }
+
 
 startButton.addEventListener("click", startQuiz)
+initialButton.addEventListener("click", function(event) {
+    event.preventDefault
+    openLeaderBoard();
+    saveUserScore();
+    renderLastScore();
+});
